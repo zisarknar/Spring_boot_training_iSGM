61c3863c11fb34c55d4a45806f59549d202ec701
Sargon Zi <iamsargonzi@gmail.com>
2019-05-02 20:55:33 +0630

    initial commit

---
+++ src/main/java/com/isgm/spring/SpringBootTrainingISgmApplication.java
@@ -0,0 +1,13 @@
+package com.isgm.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTrainingISgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTrainingISgmApplication.class, args);
+	}
+
+}
